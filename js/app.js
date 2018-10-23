51f4cd509d8075c433f06e0a404e3bf876727be7
Ahmed Elaguab <elaguabahmed97@gmail.com>
2018-10-23 17:41:34 +0200

    Display the card's symbol

--- js/app.js
+++ js/app.js
@@ -50,12 +50,30 @@ deck.appendChild(fragment)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+// The *list* of "open" cards
+var openCards = [];
+
+// Display the card's symbol function
+function displayCard(clickedCard) {
+
+    if (openCards.length < 2) {
+
+        clickedCard.classList.add('open', 'show');
+
+    }
+
+}
 
 // Set up the event listener for a card
 deck.addEventListener('click', function (e) {
 
     // If a card is clicked
     if (e.target.nodeName === "LI" && !e.target.classList.contains('open') && !e.target.classList.contains('show')) {
+
+        var clickedCard = e.target;
+
+        // Display the card's symbol
+        displayCard(clickedCard);
 
     }
 
