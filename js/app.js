f4606b10e5697151f09fc23c80c2e9cd66c7007c
Ahmed Elaguab <elaguabahmed97@gmail.com>
2018-10-23 17:25:19 +0200

    Set up the event listener for a card

--- js/app.js
+++ js/app.js
@@ -50,3 +50,13 @@ deck.appendChild(fragment)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+// Set up the event listener for a card
+deck.addEventListener('click', function (e) {
+
+    // If a card is clicked
+    if (e.target.nodeName === "LI" && !e.target.classList.contains('open') && !e.target.classList.contains('show')) {
+
+    }
+
+})
